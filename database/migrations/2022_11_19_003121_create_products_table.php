d0a1904d98a2b59a8ec944854ba9677e2bf04316
Mario Ernesto Carranza <mario.carranza996@gmail.com>
2022-11-20 16:56:51 -0600

    without imgs

--- database/migrations/2022_11_19_003121_create_products_table.php
+++ database/migrations/2022_11_19_003121_create_products_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('description');
             $table->float('price');
-            $table->text('img')->nullable()->default('https://images.unsplash.com/photo-1651950519238-15835722f8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60');
+            $table->text('img')->nullable();
 
             $table->timestamps();
         });
